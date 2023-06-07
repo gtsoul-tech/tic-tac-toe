a68eb2df0c8d695a1c8fb774b0522cd5109df9e6
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-06-07 16:30:35 +0300

    win message

--- script.js
+++ script.js
@@ -23,10 +23,20 @@ const displayController = (()=> {
     const turn = (index) => {
 
         if(gameboard.board[index] == ""){
-            
             gameboard.played(this.player.symbol,index);
 
-            console.log(check());
+            let result =check();
+            if(result === 1){
+                console.log(this.player);
+                const players = document.getElementById("players");
+                const playersText= document.querySelector(".playersText");
+                playersText.textContent = this.player.name + " won!!!";
+                players.style.display = "flex";
+
+                const startGame = document.getElementById("start");
+                startGame.style.display = "block";
+                //reset
+            }
             //based on the result show the winner for 5 sec and then restart button
             if(this.player === player1){
                 this.player=player2;
@@ -36,28 +46,28 @@ const displayController = (()=> {
             render();
         }
     };
-    
+    //1 win //2 tie
     const check = ()=>{
 
         for(let i=0;i<=6;i=i+3){
             if(gameboard.board[i] === gameboard.board[i+1] && gameboard.board[i+1] === gameboard.board[i+2] && gameboard.board[i] != ""){
-                return this.player.name + " won";
+                return 1;
             }
         }
         for(let i=0;i<=2;i=i+1){
             if(gameboard.board[i] === gameboard.board[i+3] && gameboard.board[i+3] === gameboard.board[i+6] && gameboard.board[i] != ""){ // 0 1 2
-                return this.player.name + " won";                                                                               // 3 4 5
+                return 1;                                                                               // 3 4 5
                                                                                                               // 6 7 8
             }
         }
         if(gameboard.board[0] === gameboard.board[4] && gameboard.board[4] === gameboard.board[8] && gameboard.board[0] != ""){
-            return this.player.name + " won";
+            return 1;
         }
         if(gameboard.board[2] === gameboard.board[4] && gameboard.board[4] === gameboard.board[6] && gameboard.board[2] != ""){
-            return this.player.name + " won";
+            return 1;
         }
         if(gameboard.board.join("").length === 9){
-            return "It is a tie";
+            return 2;
         }
         return "Continue";
     };
@@ -69,16 +79,16 @@ const displayController = (()=> {
         const name1 =document.getElementById("player1");
         const name2 =document.getElementById("player2");
         const players = document.getElementById("players");
-        players.textContent = `Player X (${name1.value}) VS Player O (${name2.value}) `;
-
+        const playersText= document.querySelector(".playersText");
+        playersText.textContent = `Player X (${name1.value}) VS Player O (${name2.value}) `;
         players.style.display = "flex";
         
         const startGame = document.getElementById("start");
         startGame.style.display = "none";
-        startGame.reset();
         player1 = playerFactory(name1.value, "X");
         player2 = playerFactory(name2.value, "O");
         this.player=player1;
+        startGame.reset();
         const squares = document.querySelectorAll(".square");
         squares.forEach((square,index)=> {
             square.addEventListener("click", () => {
