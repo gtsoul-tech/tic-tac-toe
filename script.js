3af0a4463c72b539e71bae96b5c30132b52356c7
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-06-08 17:36:21 +0300

    trying to add minimax

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const gameboard = (() => {
-    const board = ["","","","","","","","",""];
+    let board = ["","X","","X","O","","","",""];
+    let numberBoard = [];
+    const fillNumberBoard = ()=>{
+        board.forEach((element,index)=>{
+            if(element===""){
+                numberBoard[index]=index;
+            }else{
+                numberBoard[index]=board[index];
+            }
+        });
+        return numberBoard
+    }
+    
     const played = (symbol,index) =>{
         board[index] = symbol;
     };
-
-    return { board,played };
+    return { board,played,fillNumberBoard};
 })();
 
 const playerFactory = (name,symbol) => {
@@ -43,15 +54,35 @@ const displayController = (()=> {
                 //const startGame = document.getElementById("start");
                 //startGame.style.display = "block";
                 //reset
+            }else if(result === 2){
+                const players = document.getElementById("players");
+                const playersText= document.querySelector(".playersText");
+                if( !playersText.textContent.includes("won")){
+                    playersText.textContent ="Its a tie";
+                    players.style.display = "flex";
+                    if(this.player === player1){
+                        this.player=player2;
+                    }else if(this.player === player2){
+                        this.player=player1;
+                    }
+                    render();
+                }
             }else{
                 if(this.player === player1){
                     this.player=player2;
+                    if(this.player.name.includes("Computer")){
+                        //turn(minimax(gameboard.board,this.player.symbol,player1.symbol).index);
+                    }
                 }else if(this.player === player2){
                     this.player=player1;
+                    if(this.player.name.includes("Computer")){
+                        //turn(minimax(gameboard.board,this.player.symbol,player2.symbol).index);
+                    }
                 }
+
+                
                 render();
             }
-            //based on the result show the winner for 5 sec and then restart button
             
         }
     };
@@ -100,14 +131,26 @@ const displayController = (()=> {
         const players = document.getElementById("players");
         const playersText= document.querySelector(".playersText");
 
-        playersText.textContent = `Player X (${name1.value}) VS Player O (${name2.value}) `;
+        
+        const computerX=document.getElementById("computerX");
+        const computerO =document.getElementById("computerO");
+        if(computerX.checked){
+            player1 = playerFactory(name1.value + " Computer", "X");
+            player2 = playerFactory(name2.value, "O");
+        }else if(computerO.checked){
+            player1 = playerFactory(name1.value, "X");
+            player2 = playerFactory(name2.value + " Computer", "O");
+        }else{
+            player1 = playerFactory(name1.value, "X");
+            player2 = playerFactory(name2.value, "O");
+        }
+        playersText.textContent = `Player X (${player1.name}) VS Player O (${player2.name}) `;
         players.style.display = "flex";
         
+
+        this.player=player1;
         const startGame = document.getElementById("start");
         startGame.style.display = "none";
-        player1 = playerFactory(name1.value, "X");
-        player2 = playerFactory(name2.value, "O");
-        this.player=player1;
         startGame.reset();
         const squares = document.querySelectorAll(".square");
         squares.forEach((square,index)=> {
@@ -117,13 +160,92 @@ const displayController = (()=> {
             });
             square.textContent = gameboard.board[index];
         });
+        if(player1.name.includes("Computer")){          //edw emeinaaaaa
+            let bestSPot = minimax(gameboard.fillNumberBoard(),this.player.symbol,player2.symbol);
+            console.log(bestSPot.index);
+            turn(bestSPot.index);
+        }
+        
     };
 
     return { start,turn,reset };
 })();
+
 //displayController.start();
+function emptyIndex(board){
+  return  board.filter(index => index === "");
+};
+function winning(board, player){
+ if (
+ (board[0] == player && board[1] == player && board[2] == player) ||
+ (board[3] == player && board[4] == player && board[5] == player) ||
+ (board[6] == player && board[7] == player && board[8] == player) ||
+ (board[0] == player && board[3] == player && board[6] == player) ||
+ (board[1] == player && board[4] == player && board[7] == player) ||
+ (board[2] == player && board[5] == player && board[8] == player) ||
+ (board[0] == player && board[4] == player && board[8] == player) ||
+ (board[2] == player && board[4] == player && board[6] == player)
+ ) {
+    return true;
+ } else {
+    return false;
+ }
+}
+function minimax(board,player,human){
+    var availableSpots = emptyIndex(board);
+    
+    if (winning(board, human)){
+        return {score:-10};
+    }else if (winning(board, this.player.symbol)){
+        return {score:10};
+	}else if (availableSpots.length === 0){
+  	    return {score:0};
+    }
+    var moves = [];
+    for (let i = 0; i < availableSpots.length; i++){
+        var move = {};
+  	    move.index = board[availableSpots[i]];
+        board[availableSpots[i]] = player;
+        if (player == this.player.symbol){
+            var result = minimax(board, human,human);
+            move.score = result.score;
+        }
+        else{
+            var result = minimax(board, this.player.symbol,human);
+            move.score = result.score;
+        }
+
+        board[availableSpots[i]] = move.index;
+
+        moves.push(move);
+    }
+  var bestMove;
+  if(player === this.player.symbol){
+    var bestScore = -10000;
+    for(let i = 0; i < moves.length; i++){
+      if(moves[i].score > bestScore){
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }else{
+
+// else loop over the moves and choose the move with the lowest score
+    var bestScore = 10000;
+    for(let i = 0; i < moves.length; i++){
+      if(moves[i].score < bestScore){
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+// return the chosen move (object) from the moves array
+  return moves[bestMove];
+};
 
 const startGame = document.getElementById("start");
 startGame.addEventListener("submit", displayController.start,false);
 //console.log(gameboard.board);
 //console.log(gameboard);
+console.log(gameboard.fillNumberBoard());
